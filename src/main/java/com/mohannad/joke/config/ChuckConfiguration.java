5d43c23d9b222019f26806ee3f2cf294fda22dc0
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-05-15 23:42:10 +0200

    add xml configuration to use bean

--- src/main/java/com/mohannad/joke/config/ChuckConfiguration.java
+++ src/main/java/com/mohannad/joke/config/ChuckConfiguration.java
@@ -4,10 +4,10 @@ import com.mohannad.joke.services.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
