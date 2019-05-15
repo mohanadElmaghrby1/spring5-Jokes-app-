ed4ed41580bdb2b3c85602e782df6b46677ef1f8
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-05-15 23:17:10 +0200

    add configuration package and refactoring to use Bean annotation

---
+++ src/main/java/com/mohannad/joke/config/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package com.mohannad.joke.config;
+
+import com.mohannad.joke.services.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
